00b4448ee04f45edd43cec1be52190186cf8a717
Jennifer Saintal <jennifersaintal2@gmail.com>
2025-06-24 22:22:31 -0400

    Array map lab

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(str) {
+    return str
+      .split(' ')
+      .map(function(word) {
+        return word[0].toUpperCase() + word.slice(1);
+      })
+      .join(' ');
+  });
 }
+
